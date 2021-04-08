3eb6b61760cee0f4c3ef6db9f904498ed335fdf2
Mirkan Caliskan <mirkancaliskan@gmail.com>
2021-04-08 16:03:52 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/friendlyCards/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.friendlyCards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/counter/view/counter_page.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:friendly_cards/counter/counter.dart';
+import 'package:friendly_cards/counter/widget/card.dart';
+import 'package:friendly_cards/l10n/l10n.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: CounterView(),
+    );
+  }
+}
+
+class CounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
